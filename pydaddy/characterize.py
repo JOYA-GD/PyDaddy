ebe467722c3fa3594428c924260d7ae0c35a937e
JOYA-GD <114058063+JOYA-GD@users.noreply.github.com>
2023-05-08 17:06:54 +0530

    Update characterize.py

--- pydaddy/characterize.py
+++ pydaddy/characterize.py
@@ -347,7 +347,7 @@ def _load_sample_data(data_path):
         └── model_data
             ├── scalar
             │   ├── pairwise.csv
-            │   ├── sde-input.csv
+            │   ├── sde-input
             │   └── ternary.csv
             └── vector
                 ├── pairwise.csv
@@ -400,7 +400,7 @@ def load_sample_dataset(name):
     data_dict = {
         'fish-data-etroplus': 'data/fish_data/ectropus.csv',
         'model-data-scalar-pairwise': 'data/model_data/scalar/pairwise.csv',
-        'model-data-scalar-sde-input': 'data/model_data/scalar/sde-input.csv',
+        'model-data-scalar-sde-input': 'data/model_data/scalar/sde-input',
         'model-data-scalar-ternary': 'data/model_data/scalar/ternary.csv',
         'model-data-vector-pairwise': 'data/model_data/vector/pairwise.csv',
         'model-data-vector-ternary': 'data/model_data/vector/ternary.csv'
